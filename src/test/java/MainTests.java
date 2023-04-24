94b986a908ed0fdf042e345b434cf52cb379d083
thousandemon <william@fairfieldco.com>
2023-04-24 16:17:05 -0400

    initial commit

---
+++ src/test/java/MainTests.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MainTests {
+
+    @Test
+    public void PassingTest(){
+
+    }
+
+    @Test
+    public void FailingTest(){
+        fail();
+    }
+}
